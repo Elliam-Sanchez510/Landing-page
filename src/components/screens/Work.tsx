c0ebe4a9275d578e416915a7e71530bc2bdb14aa
Elliam-Sanchez510 <elliamsanchez510@mail.com>
2023-11-03 12:20:32 -0600

    Creating the work section and showing the projects

--- src/components/screens/Work.tsx
+++ src/components/screens/Work.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import '../styles/Work.css';
+
+const projects = [
+  {
+    id: 1,
+    name: 'Proyecto Uno',
+    description: 'Descripción del primer proyecto.',
+    technologies: 'HTML, CSS, JavaScript, React',
+    imageUrl: 'url_de_la_imagen_del_proyecto',
+    status: 'en_desarrollo'
+  },
+  {
+    id: 2,
+    name: 'Proyecto Dos',
+    description: 'Descripción del segundo proyecto.',
+    technologies: 'Python, Django, PostgreSQL',
+    imageUrl: 'url_de_la_imagen_del_proyecto',
+    status: 'en_desarrollo'
+  }
+];
 
 const Work: React.FC = (): JSX.Element => {
   return (
-    <div>
-      <h1>Welcome to Work</h1>
+    <div className="proyectos-container">
+      <h1>Proyectos Actuales</h1>
+      {projects.map((project) => (
+        <div className="proyecto" key={project.id}>
+          <h2>{project.name}</h2>
+          <p>{project.description}</p>
+          <p>Tecnologías utilizadas: {project.technologies}</p>
+          {project.status === 'en_desarrollo' && <p>Proyecto en desarrollo</p>}
+        </div>
+      ))}
     </div>
   );
 };
