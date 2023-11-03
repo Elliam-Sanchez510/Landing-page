6fcddb73348b72568215affbb6c813651364596b
Elliam-Sanchez510 <elliamsanchez510@mail.com>
2023-11-02 20:40:01 -0600

    Creating component About

--- src/components/screens/About.tsx
+++ src/components/screens/About.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import '../styles/About.css'
+import foto from '../image/foto.png'
 
-const About: React.FC = (): JSX.Element => {
+const About = () => {
   return (
     <div>
-      <h1>Welcome to About</h1>
+      <section className="inicio" id="inicio">
+        <div className="contenido-seccion">
+          <div className="info">
+            <h1>ELLIAM SANCHEZ AGUIRRE</h1>
+            <h2>CODIFICADOR, DISEÑADOR, FRONTEND</h2>
+          </div>
+          <div className="foto">
+            <img src={foto} alt="foto" />
+          </div>
+        </div>
+      </section>
+
+      <section className="sobremi" id="sobremi">
+        <div className="contenido-seccion">
+          <h2 className="titulo-seccion">Sobre Mi</h2>
+          <h3>Detalles personales</h3>
+
+          <p className="especial">Me dedico a producir ideas creativas y originales para mis clientes</p>
+          <p className="miperfil">Soy un apasionado de la creatividad y me encanta trabajar en la generación de ideas innovadoras que satisfagan las necesidades de mis clientes. Utilizo mi experiencia y conocimientos para desarrollar soluciones creativas que destaquen y generen un impacto positivo. Mi enfoque se basa en la búsqueda constante de nuevas perspectivas y en la exploración de diferentes enfoques para encontrar las mejores soluciones.</p>
+        </div>
+      </section>
     </div>
   );
 };
