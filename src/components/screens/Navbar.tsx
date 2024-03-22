1daa6e1f7f05cfdf856af2634b2b7342c8771f72
Elliam Sánchez <elliamsanchez510@gmail.com>
2024-03-22 11:03:27 -0600

    Updating navbar

--- src/components/screens/Navbar.tsx
+++ src/components/screens/Navbar.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
-import { Link, } from 'react-router-dom';
+import React, { useState } from 'react';
 import '../styles/Navbar.css';
-import logo from '../image/foto.png';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <header>
-            <nav className="navbar">
-                <div className='logo'>
-                    <img src={logo} alt="Logo" className="logo" />
-                </div>
-                <ul className="navbar-menu">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/work">Work</Link></li>
-                    <li><Link to="/services">Services</Link></li>
-                    <li><Link to="/contact">Contacto</Link></li>
+            <div className="Logo">Elliam Sánchez</div>
+            <div className="Hamburger" onClick={toggleMenu}>
+                <div className="Line"></div>
+                <div className="Line"></div>
+                <div className="Line"></div>
+            </div>
+            <nav className={`nav-bar ${isMenuOpen ? 'active' : ''}`}>
+                <ul>
+                    <li><a href="/" className="crive">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/work">Work</a></li>
+                    <li><a href="/services">Services</a></li>
+                    <li><a href="/contact">Contact</a></li>
                 </ul>
             </nav>
         </header>
